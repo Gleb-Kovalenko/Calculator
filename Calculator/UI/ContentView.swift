043322d4fc3f9f3b3bab5bda7e7b7e91de8a26de
Gleb-Kovalenko <yourpendos@gmail.com>
2022-07-29 17:15:23 +0300

    update view model and content view

--- Calculator/UI/ContentView.swift
+++ Calculator/UI/ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 // MARK: - ContentView
 
 struct ContentView: View {
-
+    
     // MARK: - Properties
     
     @StateObject private var viewModel = ViewModel()
@@ -17,7 +17,7 @@ struct ContentView: View {
         [.digit("1"), .digit("2"), .digit("3"), .addition],
         [.digit("0"), .point, .clearLast, .equal]
     ]
-
+    
     // MARK: - View
     
     var body: some View {
@@ -26,7 +26,7 @@ struct ContentView: View {
             VStack {
                 Spacer()
                 VStack(alignment: .trailing) {
-                    Text(viewModel.calculatorDisplay)
+                    viewModel.displayTextView()
                         .bold()
                         .font(.system(size: LayoutConstants.fontSizeCalculatorString))
                         .foregroundColor(.white)
@@ -36,14 +36,14 @@ struct ContentView: View {
                 }
                 ForEach(buttons.indices, id: \.self) { index in
                     HStack(spacing: 12) {
-                        ForEach(buttons[index], id: \.title) { item in
+                        ForEach(buttons[index], id: \.title) { button in
                             Button(action: {
-                                viewModel.addAction(of: item)
+                                viewModel.buttonTapped(button: button)
                             }, label: {
-                                Text(item.title)
+                                Text(button.title)
                                     .font(.system(size: LayoutConstants.fontSizeButton, weight: .bold))
-                                    .frame(maxWidth: .infinity, minHeight: 50, maxHeight: item.buttonHeight)
-                                    .background(RoundedRectangle(cornerRadius: 25, style: .continuous).fill(item.buttonColor))
+                                    .frame(maxWidth: .infinity, minHeight: 50, maxHeight: buttonHeight(shape: viewModel.buttonShape))
+                                    .background(buttonBackgroundStyle(shape: viewModel.buttonShape, button: button))
                                     .foregroundColor(.white)
                                     .cornerRadius(50)
                                     .minimumScaleFactor(0.3)
@@ -55,6 +55,27 @@ struct ContentView: View {
             }
         }
     }
+    
+    // MARK: - Private
+    
+    private func buttonBackgroundStyle(shape: ButtonShape, button: CalculatorButton) -> some View {
+        switch shape {
+        case .circle:
+            return AnyView(Circle().fill(LayoutConstants.buttonColor(button: button)))
+        case .rounded:
+            return AnyView(RoundedRectangle(cornerRadius: 25, style: .continuous).fill(LayoutConstants.buttonColor(button: button)))
+        }
+    }
+    
+    private func buttonHeight(shape: ButtonShape) -> CGFloat {
+        switch shape {
+        case .circle:
+            return (UIScreen.main.bounds.height / 2.4 - 5 * 10) / 4
+        case .rounded:
+            return (UIScreen.main.bounds.height / 2.7 - 5 * 10) / 4
+        }
+    }
+    
 }
 
 // MARK: - ContentView_Previews
@@ -70,4 +91,17 @@ struct ContentView_Previews: PreviewProvider {
 private enum LayoutConstants {
     static let fontSizeButton = UIScreen.main.bounds.width * 0.06
     static let fontSizeCalculatorString = UIScreen.main.bounds.width * 0.2
+    
+    static func buttonColor(button: CalculatorButton) -> Color {
+        switch button {
+        case .equal, .addition, .multiply, .substraction, .division:
+            return .orange
+        case .clearAll, .percent, .openBracket, .closeBracket:
+            return Color(UIColor.darkGray)
+        case .sinus, .cosinus, .power, .factorial, .tangens, .squart, .arctan:
+            return .lightDarkGray
+        case .point, .digit, .clearLast:
+            return .gray
+        }
+    }
 }
--- Calculator/UI/ViewModel/ContentView-ViewModel.swift
+++ Calculator/UI/ViewModel/ContentView-ViewModel.swift
@@ -6,37 +6,75 @@
 //
 
 import Foundation
-import MapKit
+import SwiftUI
 
 // MARK: - ContentView
 
 extension ContentView {
     
+    // MARK: - CalculatorState
+    
+    enum CalculatorState {
+        case expression(String)
+        case error(Error)
+    }
+    
+    // MARK: - ButtonShape
+    
+    enum ButtonShape {
+        case rounded
+        case circle
+    }
+    
     // MARK: - ObservableObject
     
     @MainActor class ViewModel: ObservableObject {
         
-        @Published private(set) var expression = ""
-        @Published private(set) var calculatorDisplay = ""
+        @Published private(set) var calculatorState: CalculatorState = .expression("")
+        @Published private(set) var buttonShape: ButtonShape = .rounded
+        private var expression = ""
+        
+        /// RPNCalculator instance
         private var calculator = CalculatorImplementation(
             parser: MathExpressionParser(),
             converter: RPNExpressionConverter(),
             evaluator: RPNExpressionEvaluator()
         )
         
-        func addAction(of item: CalculatorButton) {
+        func buttonTapped(button: CalculatorButton) {
             do {
-                switch item {
+                switch button {
                 case .equal:
-                    expression = try item.action(expression: expression, calculator: calculator)
-                    calculatorDisplay = expression
+                    let result = try calculator.calculate(expression: expression)
+                    if result.truncatingRemainder(dividingBy: 1) == 0 {
+                        expression = String(Int(result))
+                    } else {
+                        expression = String(result)
+                    }
+                case .factorial:
+                    expression += "!"
+                case .power:
+                    expression += "^"
+                case .clearAll:
+                    expression = ""
+                case .clearLast:
+                    expression.removeLast()
                 default:
-                    expression = try item.action(expression: expression, calculator: nil)
-                    calculatorDisplay = expression
+                    expression += button.title
                 }
+                calculatorState = .expression(expression)
             } catch let error {
-                calculatorDisplay = error.localizedDescription
                 expression = ""
+                calculatorState = .error(error)
+            }
+        }
+        
+        func displayTextView() -> Text {
+            switch calculatorState {
+            case .expression(let expression):
+                return Text(expression)
+            case .error(let error):
+                return Text(error.localizedDescription)
             }
         }
     }
